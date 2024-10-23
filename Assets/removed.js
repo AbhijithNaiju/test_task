b3bc0023eb97c2ce14ff0ba7afda77410c6ef0bc
AbhijithNaiju <abhijithnaiju@gmail.com>
2024-10-23 18:56:41 +0530

    Commit

---
+++ Assets/removed.js
@@ -0,0 +1,105 @@
+// var div = document.createElement("div");
+// div.style.height = "20px";
+// div.style.top = "500px";
+// div.innerHTML = "HI";
+// width=(100/2)+"%";
+// div.style.width=width;
+// div.classList.add("time_display");
+// document.getElementById("timeStart").appendChild(div);
+
+// var div = document.createElement("div");
+// div.style.height = "20px";
+// div.style.top = "500px";
+// left=(100/2)+"%";
+// div.style.left = left;
+// div.innerHTML = "hello";
+// width=(100/2)+"%";
+// div.style.width=width;
+// div.classList.add("time_display");
+// document.getElementById("timeStart").appendChild(div);
+
+// var div = document.createElement("div");
+// div.style.height = "20px";
+// div.style.top = "600px";
+// left=(100/3)+"%";
+// div.style.left = left;
+// div.innerHTML = "hello";
+// width=(100/3)+"%";
+// div.style.width=width;
+// div.classList.add("time_display");
+// document.getElementById("timeStart").appendChild(div);
+
+
+// var div = document.createElement("div");
+// div.style.height = "20px";
+// div.style.top = "590px";
+// left=0;
+// div.style.left = left;
+// div.innerHTML = "hello";
+// width=(100/3)+"%";
+// div.style.width=width;
+// div.classList.add("time_display");
+// document.getElementById("timeStart").appendChild(div);
+
+// var div = document.createElement("div");
+// div.style.height = "20px";
+// div.style.top = "600px";
+// left=(100/3)+"%";
+// div.style.left = left;
+// div.innerHTML = "hello";
+// width=(100/3)+"%";
+// div.style.width=width;
+// div.classList.add("time_display");
+// document.getElementById("timeStart").appendChild(div);
+
+// var div = document.createElement("div");
+// div.style.height = "20px";
+// div.style.top = "620px";
+// left=(100/1.5)+"%";
+// div.style.left = left;
+// div.innerHTML = "hello";
+// width=(100/3)+"%";
+// div.style.width=width;
+// div.classList.add("time_display");
+// document.getElementById("timeStart").appendChild(div);
+// let overlapingArray=[];
+// for(i=0;i<data.length-1;i++)
+// {
+//   if(data[i].duration>Math.abs(data[i].start-data[i+1].start))
+//   {
+//     if(overlapingArray.includes(i)==false)
+//     {
+//       overlapingArray.push(i);
+//     }
+//     if(overlapingArray.includes(i+1)==false)
+//       {
+//         overlapingArray.push(i+1);
+//       }  
+//   }
+// }
+// for (i in data) {
+//     const arrayName = `array${i}`;
+//     eval(`let ${arrayName} = []`);
+//   }
+
+//   h = h + "px";
+// document.getElementsByTagName("form", "label").style.height = h;
+// document.getElementById('timeStart')
+
+
+// for(i in data)
+// {
+//   var div = document.createElement("div");
+//   height = data[i].duration + "px";
+//   div.style.height = height;
+//   startingTime=data[i].start+"px";
+//   div.style.top = startingTime;
+//   div.innerHTML = data[i].title;
+//   div.classList.add("time_display");
+//   document.getElementById("timeStart").appendChild(div);
+// }
+
+// for(i in data)
+// {
+
+// }
